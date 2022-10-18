a261d7605f51bfaaa983df25edd3c214c52ed609
esru13 <esraelb313@gmail.comgit config --global user.name esru13>
2022-10-18 11:50:00 -0700

    team work

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+fndef MAIN_H
 
 #define MAIN_H
 
@@ -39,24 +39,24 @@
 
 
 /**
-
- * struct fmt - Struct op
-
  *
-
- * @fmt: The format.
-
- * @fn: The function associated.
-
- */
+ *  * struct fmt - Struct op
+ *
+ *   *
+ *
+ *    * @fmt: The format.
+ *
+ *     * @fn: The function associated.
+ *
+ *      */
 
 struct fmt
 
 {
 
-	char fmt;
+		char fmt;
 
-	int (*fn)(va_list, char[], int, int, int, int);
+			int (*fn)(va_list, char[], int, int, int, int);
 
 };
 
@@ -65,16 +65,16 @@ struct fmt
 
 
 /**
-
- * typedef struct fmt fmt_t - Struct op
-
  *
-
- * @fmt: The format.
-
- * @fm_t: The function associated.
-
- */
+ *  * typedef struct fmt fmt_t - Struct op
+ *
+ *   *
+ *
+ *    * @fmt: The format.
+ *
+ *     * @fm_t: The function associated.
+ *
+ *      */
 
 typedef struct fmt fmt_t;
 
@@ -84,7 +84,7 @@ int _printf(const char *format, ...);
 
 int handle_print(const char *fmt, int *i,
 
-va_list list, char buffer[], int flags, int width, int precision, int size);
+		va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
 
@@ -96,15 +96,15 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 int print_char(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int print_string(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int print_percent(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 
 
@@ -112,33 +112,33 @@ int print_percent(va_list types, char buffer[],
 
 int print_int(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int print_binary(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int print_unsigned(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int print_octal(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int print_hexadecimal(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int print_hexa_upper(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 
 
 int print_hexa(va_list types, char map_to[],
 
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
+		char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 
 
@@ -146,7 +146,7 @@ char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 int print_non_printable(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 
 
@@ -154,7 +154,7 @@ int print_non_printable(va_list types, char buffer[],
 
 int print_pointer(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 
 
@@ -174,7 +174,7 @@ int get_size(const char *format, int *i);
 
 int print_reverse(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 
 
@@ -182,7 +182,7 @@ int print_reverse(va_list types, char buffer[],
 
 int print_rot13string(va_list types, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 
 
@@ -190,27 +190,27 @@ int print_rot13string(va_list types, char buffer[],
 
 int handle_write_char(char c, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int write_number(int is_positive, int ind, char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 int write_num(int ind, char bff[], int flags, int width, int precision,
 
-	int length, char padd, char extra_c);
+			int length, char padd, char extra_c);
 
 int write_pointer(char buffer[], int ind, int length,
 
-	int width, int flags, char padd, char extra_c, int padd_start);
+			int width, int flags, char padd, char extra_c, int padd_start);
 
 
 
 int write_unsgnd(int is_negative, int ind,
 
-char buffer[],
+		char buffer[],
 
-	int flags, int width, int precision, int size);
+			int flags, int width, int precision, int size);
 
 
 
@@ -231,3 +231,4 @@ long int convert_size_unsgnd(unsigned long int num, int size);
 
 
 #endif /* MAIN_H */
+
